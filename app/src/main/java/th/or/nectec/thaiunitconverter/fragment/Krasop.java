a62762c469a676363bce0250ce9fcee2eaf15474
icesuxx <ladawan_k@hotmail.co.th>
2015-10-13 14:45:18 +0700

    ทำการเลือกขนาด และกรอกจำนวน เพื่อใช้คำนวณหาน้ำหนักข้าวสด เป็นหน่วยกิโลกรัม

--- app/src/main/java/th/or/nectec/thaiunitconverter/fragment/Krasop.java
+++ app/src/main/java/th/or/nectec/thaiunitconverter/fragment/Krasop.java
@@ -3,45 +3,38 @@ package th.or.nectec.thaiunitconverter.fragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import th.or.nectec.thaiunitconverter.R;
-
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import th.or.nectec.thaiunitconverter.R;
 import th.or.nectec.thaiunitconverter.activity.ThaiUnitCalculator;
-
 import java.util.ArrayList;
 
 /**
  * Created by User on 7/10/2558.
  */
 
-public class Krasop extends Fragment {
+public class Krasop extends Fragment implements View.OnClickListener {
 
     public static final int STANDARD_PERCENT = 15;
     private Button wetRiceButton;
     private Button dryRiceButton;
     private EditText riceQuantity;
     private EditText humidQuantity;
+    private LinearLayout krasop30, krasop50, krasop100;
     private double wetRiceValue;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.krasop);
 
-        numberView = (EditText) getView().findViewById(R.id.rice_quantity);
-        buttonView = (Button) getView().findViewById(R.id.calculate_wet_button);
-        krasop = (LinearLayout) getView().findViewById(R.id.krasop_30kg);
-        krasop = (LinearLayout) getView().findViewById(R.id.krasop_50kg);
-        krasop = (LinearLayout) getView().findViewById(R.id.krasop_100kg);
+
     }
 
     public static Krasop newInstance() {
@@ -54,23 +47,69 @@ public class Krasop extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.krasop, container, false);
+        View rootView = inflater.inflate(R.layout.krasop, container, false);
+        initInstances(rootView);
+        return rootView;
+
     }
 
-    private double calculateWetResult(int selectedid, double value) {
+    private void initInstances(View rootView) {
+        riceQuantity = (EditText) rootView.findViewById(R.id.rice_quantity);
+        wetRiceButton = (Button) rootView.findViewById(R.id.calculate_wet_button);
+        krasop30 = (LinearLayout) rootView.findViewById(R.id.krasop_30kg);
+        krasop50 = (LinearLayout) rootView.findViewById(R.id.krasop_50kg);
+        krasop100 = (LinearLayout) rootView.findViewById(R.id.krasop_100kg);
+    }
+
+    int unitFactor = 0;
+
+    @Override
+    public void onClick(View view) {
         double wetRiceValue = 0;
         // Check which radio button was clicked
-        switch (selectedid) {
-            case R.id.kasop_30kg:
-                wetRiceValue = ThaiUnitCalculator.calculateKrasobpuiToKg(value);
+        switch (view.getId()) {
+            case R.id.krasop_30kg:
+                unitFactor = 30;
+                //wetRiceValue = ThaiUnitCalculator.calculateKrasobpuiToKg(value);
                 break;
-            case R.id.kasop_50kg:
-                wetRiceValue = ThaiUnitCalculator.calculateKrasobtonnToKg(value);
+            case R.id.krasop_50kg:
+                unitFactor = 50;
+                //wetRiceValue = ThaiUnitCalculator.calculateKrasobtonnToKg(value);
                 break;
-            case R.id.kasop_100kg:
-                wetRiceValue = ThaiUnitCalculator.calculateKrasobToKg(value);
+            case R.id.krasop_100kg:
+                unitFactor = 100;
+                //wetRiceValue = ThaiUnitCalculator.calculateKrasobToKg(value);
+                break;
+            case R.id.calculate_wet_button:
+                wetRiceValue = calculateWetRice(unitFactor, riceQuantity);
+            }
+        }
+
+    private double calculateWetRice(int unitFactor, EditText riceQuantityEditText) {
+        double riceQuantityValue = 0;
+        String riceQuantityValueStr = riceQuantityEditText.getText().toString();
+
+        if (TextUtils.isEmpty(riceQuantityValueStr)) {
+            riceQuantityValue =0;
+        } else {
+            riceQuantityValue = Double.valueOf(riceQuantityValueStr);
+        }
+        double wetRiceValue = 0;
+        switch (unitFactor) {
+            case 30:
+                //unitFactor = 30;
+                wetRiceValue = ThaiUnitCalculator.calculateKrasobpuiToKg(riceQuantityValue);
+                break;
+            case 50:
+                //unitFactor = 50;
+                wetRiceValue = ThaiUnitCalculator.calculateKrasobtonnToKg(riceQuantityValue);
+                break;
+            case 100:
+                //unitFactor = 100;
+                wetRiceValue = ThaiUnitCalculator.calculateKrasobToKg(riceQuantityValue);
                 break;
         }
         return wetRiceValue;
     }
 }
+
