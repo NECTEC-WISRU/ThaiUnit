b4a4c76ed389c6385b8cb1aa51f6e04bb68b8a0d
icesuxx <ladawan_k@hotmail.co.th>
2015-10-13 16:44:52 +0700

    คำนวณในหน่วยของถัง และปรับปรุง Layout tung

--- app/src/main/java/th/or/nectec/thaiunitconverter/fragment/Tung.java
+++ app/src/main/java/th/or/nectec/thaiunitconverter/fragment/Tung.java
@@ -1,109 +1,109 @@
 package th.or.nectec.thaiunitconverter.fragment;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import th.or.nectec.thaiunitconverter.R;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import th.or.nectec.thaiunitconverter.activity.ThaiUnitCalculator;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Tung.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Tung#newInstance} factory method to
- * create an instance of this fragment.
+ * Created by User on 7/10/2558.
  */
-public class Tung extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class Tung extends Fragment implements View.OnClickListener{
+    public static final int STANDARD_PERCENT = 15;
+    private Button wetRiceButton;
+    private Button dryRiceButton;
+    private EditText riceQuantity;
+    private EditText humidQuantity;
+    private TextView sumaryView;
+    private LinearLayout tung10, tung15;
+    private double wetRiceValue;
 
-    private OnFragmentInteractionListener mListener;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Tung.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Tung newInstance(String param1, String param2) {
+
+    }
+
+    public static Tung newInstance() {
         Tung fragment = new Tung();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
-    public Tung() {
-        // Required empty public constructor
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(th.or.nectec.thaiunitconverter.R.layout.fragment_tung, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_tung, container, false);
+        initInstances(rootView);
+        return rootView;
+
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    private void initInstances(View rootView) {
+        riceQuantity = (EditText) rootView.findViewById(R.id.rice_quantity);
+        sumaryView = (TextView) rootView.findViewById(R.id.weight_wet_sumary);
+        wetRiceButton = (Button) rootView.findViewById(R.id.calculate_wet_button);
+        tung10 = (LinearLayout) rootView.findViewById(R.id.tung_10kg);
+        tung15 = (LinearLayout) rootView.findViewById(R.id.tung_15kg);
+
+        tung10.setOnClickListener(this);
+        tung15.setOnClickListener(this);
+        wetRiceButton.setOnClickListener(this);
+    }
+
+    int unitFactor = 0;
+
+    @Override
+    public void onClick(View view) {
+        double wetRiceValue = 0;
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.tung_10kg:
+                unitFactor = 10;
+                //wetRiceValue = ThaiUnitCalculator.calculateKrasobpuiToKg(value);
+                break;
+            case R.id.tung_15kg:
+                unitFactor = 15;
+                //wetRiceValue = ThaiUnitCalculator.calculateKrasobtonnToKg(value);
+                break;
+            case R.id.calculate_wet_button:
+                wetRiceValue = calculateWetRice(unitFactor, riceQuantity);
+                sumaryView.setText(unitFactor + " กิโลกรัม * " + riceQuantity.getText().toString() + " ถัง = " + wetRiceValue + " กิโลกรัม");
         }
     }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
+    private double calculateWetRice(int unitFactor, EditText riceQuantityEditText) {
+        double riceQuantityValue = 0;
+        String riceQuantityValueStr = riceQuantityEditText.getText().toString();
+
+        if (TextUtils.isEmpty(riceQuantityValueStr)) {
+            riceQuantityValue =0;
+        } else {
+            riceQuantityValue = Double.valueOf(riceQuantityValueStr);
         }
+        double wetRiceValue = 0;
+        switch (unitFactor) {
+            case 10:
+                //unitFactor = 30;
+                wetRiceValue = ThaiUnitCalculator.calculatetung10ToKg(riceQuantityValue);
+                break;
+            case 15:
+                //unitFactor = 50;
+                wetRiceValue = ThaiUnitCalculator.calculatetung15ToKg(riceQuantityValue);
+                break;
+        }
+        return wetRiceValue;
     }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
-
 }
+
