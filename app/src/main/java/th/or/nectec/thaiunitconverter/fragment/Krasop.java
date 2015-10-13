1995b23909582938ef4fcccfadd3de22ab95e07c
icesuxx <ladawan_k@hotmail.co.th>
2015-10-13 13:04:37 +0700

    แปลง class krasop ให้เป็น fragment และปรับปรุง layout

--- app/src/main/java/th/or/nectec/thaiunitconverter/fragment/Krasop.java
+++ app/src/main/java/th/or/nectec/thaiunitconverter/fragment/Krasop.java
@@ -2,52 +2,75 @@ package th.or.nectec.thaiunitconverter.fragment;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.view.ViewGroup;
+
+import th.or.nectec.thaiunitconverter.R;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import th.or.nectec.thaiunitconverter.R;
+import th.or.nectec.thaiunitconverter.activity.ThaiUnitCalculator;
 
 import java.util.ArrayList;
 
 /**
  * Created by User on 7/10/2558.
  */
-public class Krasop extends Activity {
 
-    private Spinner spinner;
+public class Krasop extends Fragment {
 
-    private ArrayList<String> ThaiUnit = new ArrayList<String>();
+    public static final int STANDARD_PERCENT = 15;
+    private Button wetRiceButton;
+    private Button dryRiceButton;
+    private EditText riceQuantity;
+    private EditText humidQuantity;
+    private double wetRiceValue;
 
-    @Override
-
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.krasop);
 
-        spinner = (Spinner) findViewById(R.id.thai_spinner);
+        numberView = (EditText) getView().findViewById(R.id.rice_quantity);
+        buttonView = (Button) getView().findViewById(R.id.calculate_wet_button);
+        krasop = (LinearLayout) getView().findViewById(R.id.krasop_30kg);
+        krasop = (LinearLayout) getView().findViewById(R.id.krasop_50kg);
+        krasop = (LinearLayout) getView().findViewById(R.id.krasop_100kg);
+    }
 
-        //createThaiUnitData();
+    public static Krasop newInstance() {
+        Krasop fragment = new Krasop();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
-        // Adapter ตัวแรก
-        ArrayAdapter<String> adapterThai = new ArrayAdapter<String>
-                (this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.thai_unit));
-        spinner.setAdapter(adapterThai);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.krasop, container, false);
+    }
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(Krasop.this, getResources().getStringArray(R.array.thai_unit)[i], Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
+    private double calculateWetResult(int selectedid, double value) {
+        double wetRiceValue = 0;
+        // Check which radio button was clicked
+        switch (selectedid) {
+            case R.id.kasop_30kg:
+                wetRiceValue = ThaiUnitCalculator.calculateKrasobpuiToKg(value);
+                break;
+            case R.id.kasop_50kg:
+                wetRiceValue = ThaiUnitCalculator.calculateKrasobtonnToKg(value);
+                break;
+            case R.id.kasop_100kg:
+                wetRiceValue = ThaiUnitCalculator.calculateKrasobToKg(value);
+                break;
+        }
+        return wetRiceValue;
     }
 }
-
-
