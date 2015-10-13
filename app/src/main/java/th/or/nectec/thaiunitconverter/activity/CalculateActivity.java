2a498be0fa5031f473db9f8550e7d7acb85015d4
icesuxx <ladawan_k@hotmail.co.th>
2015-10-13 09:28:52 +0700

    เปลี่ยน class ที่ใช้ของ CalculateActivity จาก Activity เป็น AppcompatActivity ทดลองการ replace fragment

--- app/src/main/java/th/or/nectec/thaiunitconverter/activity/CalculateActivity.java
+++ app/src/main/java/th/or/nectec/thaiunitconverter/activity/CalculateActivity.java
@@ -2,6 +2,7 @@ package th.or.nectec.thaiunitconverter.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,15 +12,18 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 import th.or.nectec.thaiunitconverter.R;
+import th.or.nectec.thaiunitconverter.fragment.Krasop;
+import th.or.nectec.thaiunitconverter.fragment.Kwian;
 
 /**
  * Created by User on 7/10/2558.
  */
-public class CalculateActivity extends Activity {
+public class CalculateActivity extends AppCompatActivity {
 
     private Spinner spinner;
 
     private ArrayList<String> ThaiUnit = new ArrayList<String>();
+    private String[] unitArray;
 
     @Override
 
@@ -27,17 +31,20 @@ public class CalculateActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculate_activity);
 
+
+        unitArray = getResources().getStringArray(R.array.thai_unit);
         spinner = (Spinner) findViewById(R.id.thai_spinner);
 
         // Adapter ตัวแรก
         ArrayAdapter<String> adapterThai = new ArrayAdapter<String>
-                (this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.thai_unit));
+                (this, android.R.layout.simple_spinner_dropdown_item, unitArray);
         spinner.setAdapter(adapterThai);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-                Toast.makeText(CalculateActivity.this, getResources().getStringArray(R.array.thai_unit)[position], Toast.LENGTH_SHORT).show();
+                Toast.makeText(CalculateActivity.this, unitArray[position], Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, Kwian.newInstance()).commit();
             }
 
             @Override
