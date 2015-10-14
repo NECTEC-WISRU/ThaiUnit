cb9320ebd286c1addce7d0786fa1577ea3b4190a
icesuxx <ladawan_k@hotmail.co.th>
2015-10-14 11:19:38 +0700

    เพิ่มปุ่มเพิ่ม ลด จำนวน +,-

--- app/src/main/java/th/or/nectec/thaiunitconverter/fragment/Krasop.java
+++ app/src/main/java/th/or/nectec/thaiunitconverter/fragment/Krasop.java
@@ -7,7 +7,9 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import th.or.nectec.thaiunitconverter.R;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -24,6 +26,9 @@ public class Krasop extends Fragment implements View.OnClickListener {
     public static final int STANDARD_PERCENT = 15;
     private Button wetRiceButton;
     private Button dryRiceButton;
+    private Button plusButton;
+    private Button minusButton;
+    private Button moreOption;
     private EditText riceQuantity;
     private EditText humidQuantity;
     private TextView sumaryView;
@@ -56,6 +61,9 @@ public class Krasop extends Fragment implements View.OnClickListener {
         riceQuantity = (EditText) rootView.findViewById(R.id.rice_quantity);
         sumaryView = (TextView) rootView.findViewById(R.id.weight_wet_sumary);
         wetRiceButton = (Button) rootView.findViewById(R.id.calculate_wet_button);
+        plusButton = (Button) rootView.findViewById(R.id.plus);
+        minusButton = (Button) rootView.findViewById(R.id.minus);
+        //moreOption = (Button) rootView.findViewById(R.id.more_option);
         krasop30 = (LinearLayout) rootView.findViewById(R.id.krasop_30kg);
         krasop50 = (LinearLayout) rootView.findViewById(R.id.krasop_50kg);
         krasop100 = (LinearLayout) rootView.findViewById(R.id.krasop_100kg);
@@ -64,6 +72,10 @@ public class Krasop extends Fragment implements View.OnClickListener {
         krasop50.setOnClickListener(this);
         krasop100.setOnClickListener(this);
         wetRiceButton.setOnClickListener(this);
+        plusButton.setOnClickListener(this);
+        minusButton.setOnClickListener(this);
+        //moreOption.setOnClickListener(this);
+
     }
 
     int unitFactor = 0;
@@ -71,7 +83,7 @@ public class Krasop extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         double wetRiceValue = 0;
-        // Check which radio button was clicked
+
         switch (view.getId()) {
             case R.id.krasop_30kg:
                 unitFactor = 30;
@@ -97,15 +109,32 @@ public class Krasop extends Fragment implements View.OnClickListener {
             case R.id.calculate_wet_button:
                 wetRiceValue = calculateWetRice(unitFactor, riceQuantity);
                 sumaryView.setText(unitFactor + " กิโลกรัม * " + riceQuantity.getText().toString() + " กระสอบ = " + wetRiceValue + " กิโลกรัม");
-            }
+                break;
+            case R.id.plus:
+                int a=Integer.parseInt(riceQuantity.getText().toString());
+                int b = a + 1;
+                riceQuantity.setText(new Integer(b).toString());
+                break;
+            case R.id.minus:
+                int c =Integer.parseInt(riceQuantity.getText().toString());
+                int d = c - 1;
+                riceQuantity.setText(new Integer(d).toString());
+                if (d == 0){
+                    
+                }
+                break;
+            //case R.id.more_option:
+
+
         }
+    }
 
     private double calculateWetRice(int unitFactor, EditText riceQuantityEditText) {
         double riceQuantityValue = 0;
         String riceQuantityValueStr = riceQuantityEditText.getText().toString();
 
         if (TextUtils.isEmpty(riceQuantityValueStr)) {
-            riceQuantityValue =0;
+            riceQuantityValue = 0;
         } else {
             riceQuantityValue = Double.valueOf(riceQuantityValueStr);
         }
