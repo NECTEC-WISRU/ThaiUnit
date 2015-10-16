b3c6353283f77fc54059cd0597e4c0aadb6f6eab
icesuxx <ladawan_k@hotmail.co.th>
2015-10-16 13:26:34 +0700

    เพิ่ม method สำหรับกำหนดและการดึงค่าน้ำหนัก ปรับปรุงการแสดงเลขทศนิยม decimalFormat

--- app/src/main/java/th/or/nectec/thaiunitconverter/view/CustomWeightView.java
+++ app/src/main/java/th/or/nectec/thaiunitconverter/view/CustomWeightView.java
@@ -3,6 +3,7 @@ package th.or.nectec.thaiunitconverter.view;
 /**
  * Created by User on 14/10/2558.
  */
+
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
@@ -14,13 +15,18 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 import th.or.nectec.thaiunitconverter.R;
 
 public class CustomWeightView extends RelativeLayout {
 
     private TextView weightText;
     private ImageView weightIcon;
-    boolean  checked = false;
+    boolean checked = false;
+    double weightFactor = 0;
 
     public CustomWeightView(Context context) {
         super(context);
@@ -56,42 +62,60 @@ public class CustomWeightView extends RelativeLayout {
 
     }
 
-    public void setCustomWeightInfo(String weightTextStr, Drawable weightIconDrawable){
-        weightText.setText(weightTextStr);
+    public void setCustomWeightInfo(String weightTextStr, Drawable weightIconDrawable, double weightFactor) {
+        this.weightFactor = weightFactor;
+
+        String pattern = "###,###.##";
+        DecimalFormat df = new DecimalFormat(pattern);
+        String output = df.format(weightFactor);
+
+        weightText.setText(weightTextStr + " (" + df.format(weightFactor) + " กก.)");
         weightIcon.setImageDrawable(weightIconDrawable);
     }
 
-    public void setCustomWeightInfoByResource(@StringRes int weightTextId, @DrawableRes int weightIconId){
+    public void setCustomWeightInfoByResource(@StringRes int weightTextId, @DrawableRes int weightIconId, double weightFactor) {
         String weightTextStr = getResources().getString(weightTextId);
         Drawable weightIconDrawable = getResources().getDrawable(weightIconId);
 
-        setCustomWeightInfo(weightTextStr, weightIconDrawable);
+        setCustomWeightInfo(weightTextStr, weightIconDrawable, weightFactor);
     }
-    public void setCustomWeightText(String weightTextStr){
+
+    public void setCustomWeightText(String weightTextStr) {
         weightText.setText(weightTextStr);
     }
 
-    public void setCustomWeightText(@StringRes int weightTextId){
+    public void setCustomWeightText(@StringRes int weightTextId) {
         weightText.setText(weightTextId);
     }
-    public void setCustomWeightDrawable(Drawable weightIconDrawable){
+
+    public void setCustomWeightDrawable(Drawable weightIconDrawable) {
         weightIcon.setImageDrawable(weightIconDrawable);
     }
-    public void setCustomWeightDrawable(@DrawableRes int weightIconId){
+
+    public void setCustomWeightDrawable(@DrawableRes int weightIconId) {
         weightIcon.setImageResource(weightIconId);
     }
 
-    public void setChecked(boolean checked){
+    public void setChecked(boolean checked) {
         this.checked = checked;
-        if (checked){
+        if (checked) {
             this.setBackgroundColor(getResources().getColor(R.color.light_gray));
-        }else{
+        } else {
             this.setBackgroundColor(Color.TRANSPARENT);
         }
     }
 
-    public  boolean isChecked(){
-        return  checked;
+    public boolean isChecked() {
+        return checked;
     }
+
+    public double getWeightFactor() {
+        return weightFactor;
+    }
+
+    public void setWeightFactor(double weightFactor) {
+        this.weightFactor = weightFactor;
+    }
+
 }
 
